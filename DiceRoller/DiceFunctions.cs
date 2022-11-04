9a6534934cff78c7cedd73574dfbff0599ee6dec
pisithsork <55104592+pisithsork@users.noreply.github.com>
2022-11-04 16:13:15 -0400

    Updated to latest version

---
+++ DiceRoller/DiceFunctions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DiceRoller
+{
+
+	public class DiceFunctions
+	{
+		public DiceFunctions()
+		{
+		}
+
+
+        public static void start(string[] SplitThis)
+        {
+            string first, second;
+            int FirstValue, SecondValue;
+            for (int i = 1; i < SplitThis.Length; i++)
+            {
+                first = SplitThis[i].Substring(0, 1);
+
+                second = SplitThis[i].Substring(2);
+
+                FirstValue = Int32.Parse(first);
+
+                SecondValue = Int32.Parse(second);
+
+                rolldice(FirstValue, SecondValue);
+            }
+            
+        }
+
+        public static void rolldice(int num, int type)
+        {
+            int sumtotal = 0;
+            string userinput;
+            var rand = new Random();
+
+            for (int i = 1; i <= num; i++)
+            {
+
+                sumtotal += rand.Next(1, (type+1));
+
+
+            }
+
+            Console.WriteLine("{0}d{1} Result: {2}", num, type, sumtotal);
+        }
+    }
+}
--- DiceRoller/DiceRoller.cs
+++ DiceRoller/DiceRoller.cs
@@ -1,72 +1,79 @@
 using System;
+using System.Linq;
 
+namespace DiceRoller
+{
+    public class Program
+    {
 
+        public static void Main()
+        {
 
-public class Program{
+            bool looping = true;
+            //! May be required to split properly
+            
+            int total = 0;
+            DiceFunctions play1 = new DiceFunctions();
 
-    public static void Main(){
+            //have the user input what they would like to roll, based off of the formula that I need to set for
+            //formula: #d#+#d#+...
+            Console.WriteLine("Welcome to dice roller, here you can roll whatever dice, however many time.");
+            Console.WriteLine("To do so enter what you want to roll with the following formula: #d#+#d#+...");
+            while (looping)
+            {   
+                // Needed to split properly
+                string userinput = "+";
+                userinput += Console.ReadLine();
 
-        bool looping = true;
-        string userinput;
-        //have the user input what they would like to roll, based off of the formula that I need to set for
-        //formula: #d#+#d#+...
-        Console.WriteLine("Welcome to dice roller, here you can roll whatever dice, however many time.");
-        Console.WriteLine("To do so enter what you want to roll with the following formula: #d#+#d#+...");
-        userinput = Console.ReadLine();
-        //Likely user input example: 4d6+1d8
-        //Based on the formula we need to split the two values that will be used to call our method
-        //We will also eventually call our method again if the "+" signs follows
-        int d = userinput.IndexOf('d');
-        int plus = userinput.IndexOf('+');
-        string temp = userinput.Substring(0,d);
-        string temp2 = userinput.Substring(d,plus);
-        int firstValue = Int32.Parse(temp);
-        int secondValue = Int32.Parse(temp2);
+                //Likely user input example: 4d6+1d8
 
-        Console.WriteLine(temp + " " + temp2);
-        Console.ReadKey();
+                String[] SplitThis = userinput.Split('+', StringSplitOptions.TrimEntries);
 
-        
-        // foreach(int i in inputvalue){
-        //     if(i == 'd'){
-        //         continue;
-        //     }
-        //     else if(i == '+'){
-        //         //determines how many times to recur rolldice()
-        //     }
-        //     else{
-        //         //convert into number and enter into 
-        //     }
-        // }
+                DiceFunctions.start(SplitThis);
+                Console.WriteLine("Would you like to roll again?\nPress 'Y' to continue or anything else to stop.");
+                string toContinue = Console.ReadLine().ToUpper();
+                if(toContinue == "Y")
+                {
+                    Console.WriteLine("Enter what you would like to roll and follow the formula: #d#+...");
+                }
+                else
+                {
+                    Console.WriteLine("Thank you for rolling with us!");
+                    looping = false;
+                }
+            }
 
-        
-
-    }
-
-
-
-    public static void rolldice(int num, int type){
-        int numofDice = num;
-        int typeofDice = type;
-        int sumtotal = 0;
-        string userinput;
-        var rand = new Random();
-
-        // Console.WriteLine("Enter the type of dice you want to roll: ");
-        // userinput = Console.ReadLine();
-        // typeofDice = Int32.Parse(userinput);
-
-        // Console.WriteLine("Enter the number of dice you want to roll: ");
-        // userinput = Console.ReadLine();
-        // numofDice = Int32.Parse(userinput);
-        
-        for(int i = 1; i <= numofDice; i++){
-
-            sumtotal += rand.Next(typeofDice + 1);
 
         }
 
-        Console.WriteLine("Result: " + sumtotal);
     }
 
 }
+    /*x for (int i = 1; i < SplitThis.Length; i++)
+            {
+                string first = SplitThis[i].Substring(0, 1);
+
+                string second = SplitThis[i].Substring(2);
+
+                int FirstValue = Int32.Parse(first);
+
+                int SecondValue = Int32.Parse(second);
+
+                rolldice(FirstValue, SecondValue);
+            }*/
+        /*x public static void rolldice(int num, int type)
+        {
+            int sumtotal = 0;
+            string userinput;
+            var rand = new Random();
+
+            for (int i = 1; i <= num; i++)
+            {
+
+                sumtotal += rand.Next(type + 1);
+
+            }
+
+            Console.WriteLine("{0}d{1} Result: {2}", num, type, sumtotal);
+        }*/
+
